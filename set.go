b2cf01ffbda1b2cb05f62b08039205ddd2207fac
Dmitry <dshipenok@gmail.com>
2020-08-25 10:56:50 +0300

    StringSet

---
+++ set.go
@@ -0,0 +1,53 @@
+package sset
+
+type StringSet struct {
+	m map[string]struct{}
+}
+
+// NewStringSet constructor
+func NewStringSet(values ...string) *StringSet {
+	m := make(map[string]struct{}, len(values))
+	for _, val := range values {
+		m[val] = struct{}{}
+	}
+	return &StringSet{
+		m: m,
+	}
+}
+
+func (s *StringSet) Add(values ...string) {
+	for _, value := range values {
+		s.m[value] = struct{}{}
+	}
+}
+
+func (s *StringSet) Merge(toMerge *StringSet) {
+	for val := range toMerge.m {
+		s.m[val] = struct{}{}
+	}
+}
+
+func (s *StringSet) Has(values ...string) bool {
+	for _, val := range values {
+		if _, has := s.m[val]; !has {
+			return false
+		}
+	}
+	return true
+}
+
+func (s *StringSet) Count() int {
+	return len(s.m)
+}
+
+func (s *StringSet) Slice() []string {
+	slice := make([]string, 0, len(s.m))
+	for val := range s.m {
+		slice = append(slice, val)
+	}
+	return slice
+}
+
+func (s *StringSet) Map() map[string]struct{} {
+	return s.m
+}
